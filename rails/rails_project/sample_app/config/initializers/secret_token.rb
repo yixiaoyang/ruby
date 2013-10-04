f883079911f6c175f26ffe4bd73b3ee7decca532
xiaoyang <hityixiaoyang@gmail.com>
2013-10-04 18:13:58 +0800

    1.add sample project:rails autotest, views

---
+++ rails/rails_project/sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '3d4df5660df233d4912a00b3d9cfe28ba219da2a6ae9fec4644138db5c66a0b5387e7e072836f7c9a6ddf312ff54a73d6ca9bb10ec7e93725178e84d0e2fe5f6'
