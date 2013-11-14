05e3a1fbd3cd5c9a8ce212df11a2f9666a21343d
xiaoyang <hityixiaoyang@gmail.com>
2013-11-15 00:10:47 +0800

    1.局部视图更新：将follow form移动到shared目录以便共享渲染 2.关注按钮功能：完成关注/取消关注的ajax实现。

--- rails/rails_project/sample_app/app/controllers/follow_relationships_controller.rb
+++ rails/rails_project/sample_app/app/controllers/follow_relationships_controller.rb
@@ -2,30 +2,45 @@ class FollowRelationshipsController < ApplicationController
   # 添加session相关资源module
   protect_from_forgery with: :exception
   include SessionsHelper
-  
-  
+ 
   before_action :signed_in_check
 
   def create
     begin
-    user = User.find(params[:follow_relationship][:followed_id])
-    current_user.follow!(user) unless user.nil?
+    @user = User.find(params[:follow_relationship][:followed_id])
+    current_user.follow!(@user) unless @user.nil?
     rescue ActiveRecord::RecordNotFound
       flash[:error] = "Relationship #{params[:id]} not found"
-    else  
+    else 
     end
-    redirect_to users_url
+    
+    # ajax surpport
+    # 在处理 Ajax 请求时，Rails 会自动调用文件名和动作名一样的“含有 JavaScript 的 
+    # ERb（JavaScript Embedded Ruby）”文件（扩展名为 .js.erb），例如 create.js.erb
+    # 和 destroy.js.erb
+    # 下面代码只有一行会被执行
+    respond_to do |format|
+      format.html { render 'users' }
+      format.js 
+    end
+    # redirect_to users_url
   end
 
   def destroy
     begin
-    user = FollowRelationship.find(params[:id]).followed
+    @user = FollowRelationship.find(params[:id]).followed
     rescue ActiveRecord::RecordNotFound
       flash[:error] = "Relationship #{params[:id]} not found"
     else
-      current_user.unfollow!(user)  unless user.nil?
+      current_user.unfollow!(@user)  unless @user.nil?
     end
-    redirect_to users_url
+ 
+    # ajax surpport
+    respond_to do |format|
+      format.html {  render 'users' }
+      format.js 
+    end
+    # redirect_to users_url
   end
 end
 
