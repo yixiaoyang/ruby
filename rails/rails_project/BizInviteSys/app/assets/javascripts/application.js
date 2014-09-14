3ff0253f2bc6c87f09676b25b601e039cebee2cd
yixiaoyang <hityixiaoyang@gmail.com>
2014-09-14 11:06:14 +0000

    1.Biz工程更新:调整bootstrap配置。

--- rails/rails_project/BizInviteSys/app/assets/javascripts/application.js
+++ rails/rails_project/BizInviteSys/app/assets/javascripts/application.js
@@ -10,10 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require jquery.ui.all
+//= require jquery-ui
 //= require best_in_place
 //= require_tree .
