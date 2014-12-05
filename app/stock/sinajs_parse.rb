415d11148550a58d4c9cd6b822a0ad5ac9f4b2e4
Leon <hityixiaoyang.com>
2014-12-05 12:52:43 +0000

    1.Staock脚本更新：完成新浪实时股票数据解析。

--- app/stock/sinajs_parse.rb
+++ app/stock/sinajs_parse.rb
@@ -2,14 +2,68 @@
 # -*- coding: utf-8 -*-
 
 require 'open-uri'
+require 'yaml'
 
 class StockParser
-   def initialize(prefix)
-	  @prefix=prefix
+   def initialize(ws,yamlFile)
+	  @format='%-10s %-10s %-10s %-10s %-10s %-16s %-16s %-16s %-16s %-12s %s'
+
+	  config = YAML::load(File.open(yamlFile))
+	  @yamlFile=yamlFile
+	  @ws=ws
+	  @prefix=config[@ws]['prefix']
+	  @reg=Regexp.new(config[@ws]['reg'])
+	  @code=config[@ws]['code']
    end
    
-   def parse(code)
-	  
+   def print_hdr
+	  formatStr=sprintf(@format,
+						"Last",
+						"Open",
+						"Yestdy",
+						"Chg",
+						"Chg%",
+						"Hands",
+						"Volum",
+						"Sel1",
+						"Buy1",
+						"Code", "Name")
+	  puts formatStr 
+   end
+
+   def print
+	  #todayStartP=@stockData[1].to_f
+	  yestodayEndP=@stockData[2].to_f
+	  currentP=@stockData[3].to_f
+	  chg=currentP-yestodayEndP
+	  chgStr="%.6s" % chg
+	  chgPerStr="%0.5s" % ((chg/yestodayEndP)*100)
+
+	  formatStr=sprintf(@format,
+						@stockData[3],
+						@stockData[1],
+						@stockData[2],
+						chgStr,chgPerStr,
+						@stockData[8],
+						@stockData[9],
+						@stockData[20]+"/"+@stockData[21],
+						@stockData[10]+"/"+@stockData[11],
+						@stockCode,
+						@stockData[0])
+	  puts formatStr
+   end
+   
+   def parse()
+	  url=@prefix+@code
+	  open(url){ |f|
+		 @response=f.read.to_s
+	  }
+	  @response.each_line{ |stockStr|
+		 @reg =~ stockStr
+		 @stockCode=$1
+		 @stockData=$2.split(',')
+		 self.print
+	  }
    end
 
    def test(code)
@@ -20,7 +74,6 @@ class StockParser
    end
 end
 
-code="sh601006"
-url_prefix = "http://hq.sinajs.cn/list="
-sinaParser=StockParser.new(url_prefix)
-sinaParser.test(code)
+sinaParser=StockParser.new('sina','StockParse.yaml')
+sinaParser.print_hdr
+sinaParser.parse()
